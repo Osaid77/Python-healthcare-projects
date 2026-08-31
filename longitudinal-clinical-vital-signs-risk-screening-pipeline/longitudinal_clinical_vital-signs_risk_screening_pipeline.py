378851706cbc235599c7286a887c85e3b6b69fcd
Osaid Alsisi <theworldismine77@hotmail.com>
2026-08-31 03:45:04 +0300

    Enhance error handling and observation tracking
    Added error handling for JSON loading and CSV reading. Improved variable initialization and added tracking for malformed observations.

--- longitudinal-clinical-vital-signs-risk-screening-pipeline/longitudinal_clinical_vital-signs_risk_screening_pipeline.py
+++ longitudinal-clinical-vital-signs-risk-screening-pipeline/longitudinal_clinical_vital-signs_risk_screening_pipeline.py
@@ -1,33 +1,48 @@
 import csv
 import json
 
+
 # Configure Validation and Thresholds
 
 def load_config():
-    with open("risk_config.json", "r") as file:
-        return json.load(file)
+    try:
+        with open("risk_config.json", "r") as file:
+            return json.load(file)
+
+    except json.JSONDecodeError:
+        print("Invalid json file configuration!")
+        raise    # So it stops the program immediately because this is a serious proced
+
+    except FileNotFoundError:
+        print("File not found!")
+        raise
 
 
 # Thresholds
 config = load_config()
-fever_threshold = config["thresholds"]["fever"]
-low_oxygen_threshold = config["thresholds"]["low_oxygen"]
-high_heart_rate_threshold = config["thresholds"]["high_heart_rate"]
-high_resp_rate_threshold = config["thresholds"]["high_respiratory_rate"]
-low_systolic_bp_threshold = config["thresholds"]["low_systolic_bp"]
-older_age_threshold = config["thresholds"]["older_age"]
+try:
+    fever_threshold = config["thresholds"]["fever"]
+    low_oxygen_threshold = config["thresholds"]["low_oxygen"]
+    high_heart_rate_threshold = config["thresholds"]["high_heart_rate"]
+    high_resp_rate_threshold = config["thresholds"]["high_respiratory_rate"]
+    low_systolic_bp_threshold = config["thresholds"]["low_systolic_bp"]
+    older_age_threshold = config["thresholds"]["older_age"]
 
 # Valid ranges
-min_temp = config["valid_ranges"]["temperature_min"]
-max_temp = config["valid_ranges"]["temperature_max"]
-min_oxygen = config["valid_ranges"]["oxygen_min"]
-max_oxygen = config["valid_ranges"]["oxygen_max"]
-min_heart_rate = config["valid_ranges"]["heart_rate_min"]
-max_heart_rate = config["valid_ranges"]["heart_rate_max"]
-min_resp_rate = config["valid_ranges"]["respiratory_rate_min"]
-max_resp_rate = config["valid_ranges"]["respiratory_rate_max"]
-min_systolic_bp = config["valid_ranges"]["systolic_bp_min"]
-max_systolic_bp = config["valid_ranges"]["systolic_bp_max"]
+    min_temp = config["valid_ranges"]["temperature_min"]
+    max_temp = config["valid_ranges"]["temperature_max"]
+    min_oxygen = config["valid_ranges"]["oxygen_min"]
+    max_oxygen = config["valid_ranges"]["oxygen_max"]
+    min_heart_rate = config["valid_ranges"]["heart_rate_min"]
+    max_heart_rate = config["valid_ranges"]["heart_rate_max"]
+    min_resp_rate = config["valid_ranges"]["respiratory_rate_min"]
+    max_resp_rate = config["valid_ranges"]["respiratory_rate_max"]
+    min_systolic_bp = config["valid_ranges"]["systolic_bp_min"]
+    max_systolic_bp = config["valid_ranges"]["systolic_bp_max"]
+
+except KeyError as error:       # It lets you see which key caused the error.
+    print("Key doesn't exist!", error)
+    raise
 
 
 ##############
@@ -102,7 +117,6 @@ def calculate_risk(age, heart_rate, respiratory_rate, systolic_BP, temperature, 
 
 
 def classify_risk(risk_score):
-    risk_level = "Invalid"
 
     if risk_score >= 4:
         return "High risk"
@@ -116,128 +130,153 @@ def classify_risk(risk_score):
 
 ##############
 
+try:
 
-with open("patients_data.csv", "r") as input_file:
-    patients = csv.DictReader(input_file)
+    with open("patients_data.csv", "r") as input_file:
+        patients = csv.DictReader(input_file)
 
-    with open("screening_results.csv", "w", newline="") as output_file:
-        fieldnames = ["hadm_id", "charttime", "age", "sex", "HR", "RR",
-                      "SBP", "TEMP", "SPO2", "valid_data", "fever", "low_oxygen",
-                      "high_heart_rate", "high_respiratory_rate", "low_systolic_bp", "risk_score", "risk_level"]
+        with open("screening_results.csv", "w", newline="") as output_file:
+            fieldnames = ["hadm_id", "charttime", "age", "sex", "HR", "RR",
+                          "SBP", "TEMP", "SPO2", "valid_data", "fever", "low_oxygen",
+                          "high_heart_rate", "high_respiratory_rate", "low_systolic_bp", "risk_score", "risk_level"]
 
-        writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+            writer = csv.DictWriter(output_file, fieldnames=fieldnames)
 
-        writer.writeheader()
+            writer.writeheader()
 
-        total_observations = 0
-        valid_observations = 0
-        invalid_observations = 0
-        low_risk = 0
-        moderate_risk = 0
-        high_risk = 0
-        high_risk_admissions = set()
+            total_observations = 0
+            valid_observations = 0
+            invalid_observations = 0
+            malformed_observations = 0
+            low_risk = 0
+            moderate_risk = 0
+            high_risk = 0
+            high_risk_admissions = set()
 
-        for item in patients:
+            for item in patients:
+                total_observations += 1
 
-            hospital_adm_id = item["hadm_id"]
-            age = int(item["age"])
-            gender = item["sex"]
-            chart_time = item["charttime"]
-            heart_rate = float(item["HR"])
-            respiratory_rate = float(item["RR"])
-            systolic_BP = float(item["SBP"])
-            temperature = float(item["TEMP"])
-            oxygen_sat = float(item["SPO2"])
+                try:
 
-            is_valid = validate_vitals(
-                heart_rate, respiratory_rate, systolic_BP, temperature, oxygen_sat)
+                    hospital_adm_id = item["hadm_id"]
+                    age = int(item["age"])
+                    gender = item["sex"]
+                    chart_time = item["charttime"]
+                    heart_rate = float(item["HR"])
+                    respiratory_rate = float(item["RR"])
+                    systolic_BP = float(item["SBP"])
+                    temperature = float(item["TEMP"])
+                    oxygen_sat = float(item["SPO2"])
 
-            risk_score = ""
-            risk_level = "Invalid"
+                except (ValueError, TypeError):
+                    print("Invalid numeric data!")
+                    malformed_observations += 1
+                    continue
 
-            has_elderly_age = False
-            has_high_heart_rate = False
-            has_high_resp_rate = False
-            has_low_systolic_bp = False
-            has_fever = False
-            has_low_oxygen = False
+                except KeyError as error:
+                    print("Key doesn't exist!", error)
+                    malformed_observations += 1
+                    continue
 
-            if is_valid:
+                is_valid = validate_vitals(
+                    heart_rate, respiratory_rate, systolic_BP, temperature, oxygen_sat)
 
-                (
-                    risk_score,
-                    has_elderly_age,
-                    has_high_heart_rate,
-                    has_high_resp_rate,
-                    has_low_systolic_bp,
-                    has_fever,
-                    has_low_oxygen
-                ) = calculate_risk(
-                    age,
-                    heart_rate,
-                    respiratory_rate,
-                    systolic_BP,
-                    temperature,
-                    oxygen_sat
-                )
+                risk_score = ""
+                risk_level = "Invalid"
 
-                risk_level = classify_risk(risk_score)
+                has_elderly_age = False
+                has_high_heart_rate = False
+                has_high_resp_rate = False
+                has_low_systolic_bp = False
+                has_fever = False
+                has_low_oxygen = False
 
-                valid_observations += 1
+                if is_valid:
 
-                if risk_level == "High risk":
-                    high_risk += 1
-                    high_risk_admissions.add(hospital_adm_id)
+                    (      
+                        risk_score,
+                        has_elderly_age,
+                        has_high_heart_rate,
+                        has_high_resp_rate,
+                        has_low_systolic_bp,
+                        has_fever,
+                        has_low_oxygen
 
-                elif risk_level == "Moderate risk":
-                    moderate_risk += 1
+                    ) = calculate_risk(  
+                        age,
+                        heart_rate,
+                        respiratory_rate,
+                        systolic_BP,
+                        temperature,
+                        oxygen_sat
+
+                    )
+
+                    risk_level = classify_risk(risk_score)
+
+                    valid_observations += 1
+
+                    if risk_level == "High risk":
+                        high_risk += 1
+                        high_risk_admissions.add(hospital_adm_id)
+
+                    elif risk_level == "Moderate risk":
+                        moderate_risk += 1
+
+                    else:
+                        low_risk += 1
 
                 else:
-                    low_risk += 1
+                    print("Invalid data, cannot calculate risk score and level!")
+                    invalid_observations += 1
 
-            else:
-                print("Invalid data, cannot calculate risk score and level!")
-                invalid_observations += 1
-
-            writer.writerow({
-                "hadm_id": hospital_adm_id,
-                "charttime": chart_time,
-                "age": age,
-                "sex": gender,
-                "HR": heart_rate,
-                "RR": respiratory_rate,
-                "SBP": systolic_BP,
-                "TEMP": temperature,
-                "SPO2": oxygen_sat,
-                "valid_data": is_valid,
-                "fever": has_fever,
-                "low_oxygen": has_low_oxygen,
-                "high_heart_rate": has_high_heart_rate,
-                "high_respiratory_rate": has_high_resp_rate,
-                "low_systolic_bp": has_low_systolic_bp,
-                "risk_score": risk_score,
-                "risk_level": risk_level
-            })
-
-            total_observations += 1
-
-        print()
-        print("Total ovservations:", total_observations)
-        print("Valid observations:", valid_observations)
-        print("Invalid observations:", invalid_observations)
-        print("High risk:", high_risk)
-        print("Moderate risk:", moderate_risk)
-        print("Low risk:", low_risk)
-
-        print("Admissions who had high risk:", high_risk_admissions)
-        print("Number of admissions with high risk:", len(high_risk_admissions))
+                writer.writerow({
+                    "hadm_id": hospital_adm_id,
+                    "charttime": chart_time,
+                    "age": age,
+                    "sex": gender,
+                    "HR": heart_rate,
+                    "RR": respiratory_rate,
+                    "SBP": systolic_BP,
+                    "TEMP": temperature,
+                    "SPO2": oxygen_sat,
+                    "valid_data": is_valid,
+                    "fever": has_fever,
+                    "low_oxygen": has_low_oxygen,
+                    "high_heart_rate": has_high_heart_rate,
+                    "high_respiratory_rate": has_high_resp_rate,
+                    "low_systolic_bp": has_low_systolic_bp,
+                    "risk_score": risk_score,
+                    "risk_level": risk_level
+                })
 
 
+except FileNotFoundError:
+    print("Required data file not found!")
+    raise
+
+print()
+print("Total ovservations:", total_observations)
+print("Valid observations:", valid_observations)
+print("Invalid observations:", invalid_observations)
+print("Malformed observations:", malformed_observations)
+print("High risk:", high_risk)
+print("Moderate risk:", moderate_risk)
+print("Low risk:", low_risk)
+
+print("Admissions who had high risk:", high_risk_admissions)
+print("Number of admissions with high risk:", len(high_risk_admissions))
+
+
+
+# Making a screening_summary json
 summary = {
     'dataset': {
         "total_observations": total_observations,
         "valid_observations": valid_observations,
-        "invalid_observations": invalid_observations
+        "invalid_observations": invalid_observations,
+        "Malformed observations:": malformed_observations
+
     },
     "risk_distribution": {
         "low": low_risk,
@@ -249,5 +288,7 @@ summary = {
     }
 }
 
+
 with open("screening_summary.json", "w") as file:
     json.dump(summary, file, indent=4)
+
