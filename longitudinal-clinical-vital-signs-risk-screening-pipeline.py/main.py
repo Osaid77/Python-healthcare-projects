8f398b521c1a69783ba393195cc79deb664de830
Osaid Alsisi <theworldismine77@hotmail.com>
2026-09-08 00:37:05 +0300

    Refactor risk calculation and validation logic

--- longitudinal-clinical-vital-signs-risk-screening-pipeline.py/main.py
+++ longitudinal-clinical-vital-signs-risk-screening-pipeline.py/main.py
@@ -1,135 +1,8 @@
 import csv
 import json
+from risk import calculate_risk, classify_risk
+import validation
 
-
-# Configure Validation and Thresholds
-
-def load_config():
-    try:
-        with open("risk_config.json", "r") as file:
-            return json.load(file)
-
-    except json.JSONDecodeError:
-        print("Invalid json file configuration!")
-        raise    
-
-    except FileNotFoundError:
-        print("File not found!")
-        raise
-
-
-config = load_config()
-try:
-
-# Thresholds
-    fever_threshold = config["thresholds"]["fever"]
-    low_oxygen_threshold = config["thresholds"]["low_oxygen"]
-    high_heart_rate_threshold = config["thresholds"]["high_heart_rate"]
-    high_resp_rate_threshold = config["thresholds"]["high_respiratory_rate"]
-    low_systolic_bp_threshold = config["thresholds"]["low_systolic_bp"]
-    older_age_threshold = config["thresholds"]["older_age"]
-
-# Valid ranges
-    min_temp = config["valid_ranges"]["temperature_min"]
-    max_temp = config["valid_ranges"]["temperature_max"]
-    min_oxygen = config["valid_ranges"]["oxygen_min"]
-    max_oxygen = config["valid_ranges"]["oxygen_max"]
-    min_heart_rate = config["valid_ranges"]["heart_rate_min"]
-    max_heart_rate = config["valid_ranges"]["heart_rate_max"]
-    min_resp_rate = config["valid_ranges"]["respiratory_rate_min"]
-    max_resp_rate = config["valid_ranges"]["respiratory_rate_max"]
-    min_systolic_bp = config["valid_ranges"]["systolic_bp_min"]
-    max_systolic_bp = config["valid_ranges"]["systolic_bp_max"]
-
-except KeyError as error:     
-    print("Key doesn't exist!", error)
-    raise
-
-
-##############
-
-def validate_vitals(heart_rate, respiratory_rate, systolic_BP, temperature, oxygen_sat):
-
-    is_valid = True
-
-    if not min_heart_rate <= heart_rate <= max_heart_rate:
-        print("Invalid heart rate")
-        is_valid = False
-
-    if not min_resp_rate <= respiratory_rate <= max_resp_rate:
-        print("Invalid respiratory rate")
-        is_valid = False
-
-    if not min_systolic_bp <= systolic_BP <= max_systolic_bp:
-        print("Invalid systolic bp")
-        is_valid = False
-
-    if not min_temp <= temperature <= max_temp:
-        print("Invalid temperature")
-        is_valid = False
-
-    if not min_oxygen <= oxygen_sat <= max_oxygen:
-        print("Invalid oxygen saturation")
-        is_valid = False
-
-    return is_valid
-
-#################
-
-
-def calculate_risk(age, heart_rate, respiratory_rate, systolic_BP, temperature, oxygen_sat):
-
-    risk_score = 0
-    has_elderly_age = False
-    has_high_heart_rate = False
-    has_high_resp_rate = False
-    has_low_systolic_bp = False
-    has_fever = False
-    has_low_oxygen = False
-
-    if age >= older_age_threshold:
-        has_elderly_age = True
-        risk_score += 1
-
-    if heart_rate >= high_heart_rate_threshold:
-        has_high_heart_rate = True
-        risk_score += 1
-
-    if respiratory_rate >= high_resp_rate_threshold:
-        has_high_resp_rate = True
-        risk_score += 1
-
-    if systolic_BP <= low_systolic_bp_threshold:
-        has_low_systolic_bp = True
-        risk_score += 1
-
-    if temperature >= fever_threshold:
-        has_fever = True
-        risk_score += 1
-
-    if oxygen_sat <= low_oxygen_threshold:
-        has_low_oxygen = True
-        risk_score += 1
-
-    return (risk_score, has_elderly_age, has_high_heart_rate, has_high_resp_rate, has_low_systolic_bp, has_fever, has_low_oxygen)
-
-
-########
-
-
-def classify_risk(risk_score):
-
-    if risk_score >= 4:
-        return "High risk"
-
-    elif risk_score >= 2:
-        return "Moderate risk"
-
-    else:
-        return "Low risk"
-
-
-##############
 
 try:
 
@@ -179,7 +52,7 @@ try:
                     malformed_observations += 1
                     continue
 
-                is_valid = validate_vitals(
+                is_valid = validation.validate_vitals(
                     heart_rate, respiratory_rate, systolic_BP, temperature, oxygen_sat)
 
                 risk_score = ""
@@ -194,7 +67,7 @@ try:
 
                 if is_valid:
 
-                    (      
+                    (
                         risk_score,
                         has_elderly_age,
                         has_high_heart_rate,
@@ -203,7 +76,7 @@ try:
                         has_fever,
                         has_low_oxygen
 
-                    ) = calculate_risk(  
+                    ) = calculate_risk(
                         age,
                         heart_rate,
                         respiratory_rate,
@@ -269,7 +142,6 @@ print("Admissions who had high risk:", high_risk_admissions)
 print("Number of admissions with high risk:", len(high_risk_admissions))
 
 
-
 # Dumping a screening_summary json
 summary = {
     'dataset': {
@@ -292,4 +164,5 @@ summary = {
 
 with open("screening_summary.json", "w") as file:
     json.dump(summary, file, indent=4)
+
 
